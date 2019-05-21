32b323c353cab8529da4ff445c81e04b2932f77f
michaelolsen137 <49927057+michaelolsen137@users.noreply.github.com>
2019-05-21 00:16:43 -0500

    Add files via upload

---
+++ Assignment Part 2Lullaby.rb
@@ -0,0 +1,73 @@
+# Welcome to Sonic Pi! v2.11.1
+in_thread do
+  2.times do
+    play 74
+    sleep 0.5
+    play 74
+    sleep 0.5
+    play 70
+    play 77
+    sleep 1
+  end
+  
+  play 74
+  sleep 0.5
+  play 77
+  sleep 0.5
+  play 82
+  play 70
+  play 77
+  sleep 1
+  
+  play 81
+  play 69
+  play 77
+  sleep 1
+  
+  play 79
+  play 67
+  play 77
+  sleep 1
+  
+  play 79
+  play 67
+  play 77
+  sleep 1
+  
+  play 70
+  play 65
+  play 77
+  sleep 1
+  2.times do
+    play 72
+    sleep 0.5
+    play 74
+    sleep 0.5
+    play 75
+    play 72
+    play 67
+    sleep 1
+  end
+  
+  play 72
+  sleep 0.5
+  play 75
+  sleep 0.5
+  play 75
+  play 79
+  play 82
+  sleep 1
+  play 81
+  sleep 1
+  play 75
+  play 79
+  play 70
+  sleep 1
+  play 81
+  play 77
+  play 72
+  sleep 1
+  play 74
+  play 77
+  play 82
+end
